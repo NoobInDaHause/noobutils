8f8256b38a18426e150c7dd7e3682ed512242da3
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-07-27 22:55:18 +0800

    remove path

--- noobutils/__init__.py
+++ noobutils/__init__.py
@@ -1,6 +1,4 @@
-from pathlib import Path
-
-with open(Path(__file__).parent / "version.txt", "r") as vf:
+with open("version.txt", "r") as vf:
     __version__ = vf.read()
 
 from .converters import *
