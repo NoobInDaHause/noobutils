26d865115c51ec018425975cd9d6fd74b11081db
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-10-25 15:14:34 +0800

    alright hopefully this checks if bot has no access

--- noobutils/converters.py
+++ noobutils/converters.py
@@ -1,6 +1,10 @@
-from redbot.core.commands import Context, EmojiConverter as ec
+import discord
+
+from redbot.core import commands
 
 from emoji import EMOJI_DATA
+
+from .exceptions import UnknownEmoji
 
 
 class NoobCoordinate(dict):
@@ -8,9 +12,13 @@ class NoobCoordinate(dict):
         return key
 
 
-class NoobEmojiConverter(ec):
-    async def convert(self, context: Context, argument: str):
+class NoobEmojiConverter(commands.EmojiConverter):
+    async def convert(self, context: commands.Context, argument: str):
         argument = argument.strip()
+        checkarg = argument.replace(":", "")
+        emote = discord.utils.get(context.bot.emojis, name=checkarg)
+        if emote is None:
+            raise UnknownEmoji(f'Emoji "{argument}" not found.')
         try:
             EMOJI_DATA[argument]
         except KeyError:
