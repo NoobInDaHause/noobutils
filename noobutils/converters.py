886e94075e9ce11433cd37c885a1965de2e11987
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-10-25 23:58:54 +0800

    sigh so much time wasted

--- noobutils/converters.py
+++ noobutils/converters.py
@@ -1,39 +1,3 @@
-import discord
-
-from redbot.core.bot import commands
-
-from emoji import EMOJI_DATA
-from typing import Union
-
-
 class NoobCoordinate(dict):
     def __convert__(self, key):
         return key
-
-
-async def noob_emoji_converter(
-    context: commands.Context, emoji
-) -> Union[discord.Emoji, str, None]:
-    try:
-        emoji = int(emoji)
-    except Exception:
-        emoji = emoji
-    if isinstance(emoji, str):
-        emoji = emoji.strip()
-        try:
-            EMOJI_DATA[emoji]
-            return emoji
-        except KeyError:
-            custom_emoji = emoji.split(":")
-            if len(custom_emoji) < 2:
-                return None
-            custom_emoji = custom_emoji[2].replace(">", "")
-            try:
-                d = int(custom_emoji)
-                return discord.utils.get(context.bot.emojis, id=d)
-            except Exception:
-                return None
-    elif isinstance(emoji, int):
-        return discord.utils.get(context.bot.emojis, id=emoji)
-    else:
-        return None
