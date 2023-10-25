9a0c96333b374232cd22d5c85c8dbb1f3aafe5e0
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-10-25 19:06:05 +0800

    forgot to import

--- noobutils/__init__.py
+++ noobutils/__init__.py
@@ -1,5 +1,6 @@
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
+from .converters import *
 from .exceptions import *
 from .views import *
 from .utility import *
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as f:
     long_description = f.read()
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 setup(
     name="noobutils",
