020ce92ef51567aae783ed7c6e5381108c071d89
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-10-25 15:24:20 +0800

    back

--- noobutils/converters.py
+++ noobutils/converters.py
@@ -1,10 +1,6 @@
-import discord
-
-from redbot.core.commands import Context, EmojiConverter as ec
+from redbot.core.commands import EmojiConverter as ec
 
 from emoji import EMOJI_DATA
-
-from .exceptions import UnknownEmoji
 
 
 class NoobCoordinate(dict):
@@ -12,13 +8,9 @@ class NoobCoordinate(dict):
         return key
 
 
-class NoobEmojiConverter(ec):
-    async def convert(self, ctx: Context, argument: str):
+class EmojiConverter(ec):
+    async def convert(self, ctx, argument):
         argument = argument.strip()
-        checkarg = argument.replace(":", "")
-        emote = discord.utils.get(ctx.bot.emojis, name=checkarg)
-        if emote is None:
-            raise UnknownEmoji(f'Emoji "{argument}" not found.')
         try:
             EMOJI_DATA[argument]
         except KeyError:
