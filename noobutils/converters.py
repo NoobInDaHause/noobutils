c17270c2331e3a7f2e961a386836cf3d41a008f8
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-10-25 13:50:49 +0800

    emojiconverter fix i hope

--- noobutils/converters.py
+++ noobutils/converters.py
@@ -1,17 +1,19 @@
-from redbot.core import commands
+from redbot.core.commands import Context, EmojiConverter as ec
 
 from emoji import EMOJI_DATA
+
 
 class NoobCoordinate(dict):
     def __convert__(self, key):
         return key
 
-class NoobEmojiConverter(commands.EmojiConverter):
-    async def convert(self, context: commands.Context, argument: str):
+
+class NoobEmojiConverter(ec):
+    async def convert(self, context: Context, argument: str):
         argument = argument.strip()
         try:
             EMOJI_DATA[argument]
         except KeyError:
-            return str(await super().convert(context=context, argument=argument))
+            return str(await super().convert(context, argument))
         else:
             return argument
--- noobutils/utility.py
+++ noobutils/utility.py
@@ -7,26 +7,29 @@ from typing import Union, List
 from .converters import NoobCoordinate
 from .exceptions import ButtonColourNotFound, MemberOrGuildNotFound
 
-def is_have_avatar(thing: Union[discord.Member, discord.Guild] = None, display_av = False) -> str:
+
+def is_have_avatar(
+    thing: Union[discord.Member, discord.Guild] = None, display_av=False
+) -> str:
     if thing is None:
         return ""
     elif isinstance(thing, discord.Member):
         if display_av:
             return thing.display_avatar.url or ""
         else:
-            return (thing.avatar.url or (thing.display_avatar.url or ""))
+            return thing.avatar.url or (thing.display_avatar.url or "")
     elif isinstance(thing, discord.Guild):
         return thing.icon.url or ""
     else:
-        raise MemberOrGuildNotFound(
-            f'Member or Guild "{thing}" was not found.'
-        )
+        raise MemberOrGuildNotFound(f'Member or Guild "{thing}" was not found.')
 
-def access_denied(text_only = False) -> str:
+
+def access_denied(text_only=False) -> str:
     if text_only:
         return "Access Denied."
     else:
         return "https://cdn.discordapp.com/attachments/1000751975308197918/1110013262835228814/1.mp4"
+
 
 def get_button_colour(colour: str) -> discord.ButtonStyle:
     """
@@ -41,9 +44,8 @@ def get_button_colour(colour: str) -> discord.ButtonStyle:
     elif colour.lower() == "grey":
         return discord.ButtonStyle.grey
     else:
-        raise ButtonColourNotFound(
-            f'"{colour}" is not a valid button colour.'
-        )
+        raise ButtonColourNotFound(f'"{colour}" is not a valid button colour.')
+
 
 async def pagify_this(
     big_ass_variable_string: str,
@@ -57,24 +59,32 @@ async def pagify_this(
 ) -> List[Union[discord.Embed, str]]:
     final_page: List[Union[discord.Embed, str]] = []
     if is_embed:
-        pages = list(cf.pagify(big_ass_variable_string, delims=[delim], page_length=page_char))
+        pages = list(
+            cf.pagify(big_ass_variable_string, delims=[delim], page_length=page_char)
+        )
     else:
-        pages = list(cf.pagify(big_ass_variable_string, delims=[delim], page_length=(page_char - 50)))
+        pages = list(
+            cf.pagify(
+                big_ass_variable_string, delims=[delim], page_length=(page_char - 50)
+            )
+        )
 
     for index, page in enumerate(pages, 1):
         if is_embed:
             embed = discord.Embed(
-                title=embed_title,
-                colour=embed_colour,
-                description=page
+                title=embed_title, colour=embed_colour, description=page
             ).set_footer(
-                text=page_text.format_map(NoobCoordinate(index=index, pages=len(pages))),
-                icon_url=footer_icon
+                text=page_text.format_map(
+                    NoobCoordinate(index=index, pages=len(pages))
+                ),
+                icon_url=footer_icon,
             )
             final_page.append(embed)
         else:
             t = "{page}\n\n{page_text}".format_map(
-                    NoobCoordinate(page=page, page_text=page_text, index=index, pages=len(pages))
+                NoobCoordinate(
+                    page=page, page_text=page_text, index=index, pages=len(pages)
                 )
+            )
             final_page.append(t)
     return final_page
